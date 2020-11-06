4550e7c89c28d20dc0f2f32ba195b53710bfe6c2
bishnu karki <bishnukarki1988@gmail.com>
2020-11-06 11:36:14 -0700

    Add project files.

---
+++ CalculatorLibrary.Test/TestOperations.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CalculatorLibrary.Test
+{
+    [TestClass]
+    public class TestOperations
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            //Arrange 
+            int num1 = 7;
+            int num2 = 5;
+            int expected = 12;
+
+            //Act
+            Operations op = new Operations();
+            int actual = op.Add(num1, num2);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSubstract()
+        {
+            //Arrange 
+            int num1 = 7;
+            int num2 = 5;
+            int expected = 2;
+
+            //Act
+            Operations op = new Operations();
+            int actual = op.Substract(num1, num2);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestMultiply()
+        {
+            //Arrange 
+            int num1 = 7;
+            int num2 = 5;
+            int expected = 35;
+
+            //Act
+            Operations op = new Operations();
+            int actual = op.Multipy(num1, num2);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestDivide()
+        {
+            //Arrange 
+            int num1 = 10;
+            int num2 = 5;
+            int expected = 2;
+
+            //Act
+            Operations op = new Operations();
+            int actual = op.Divide(num1, num2);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ CalculatorLibrary/Operations.cs
@@ -0,0 +1,29 @@
+﻿namespace CalculatorLibrary
+{
+    public class Operations
+    {
+        public int Add(int num1, int num2)
+        {
+            int value = num1 + num2;
+            return value;
+        }
+
+        public int Substract(int num1, int num2)
+        {
+            int value = num1 - num2;
+            return value;
+        }
+
+        public int Multipy(int num1, int num2)
+        {
+            int value = num1 * num2;
+            return value;
+        }
+
+        public int Divide(int num1, int num2)
+        {
+            int value = num1 / num2;
+            return value;
+        }
+    }
+}
